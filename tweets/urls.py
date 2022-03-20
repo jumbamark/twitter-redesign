ef326ec88817be15872a7e4dd031c339e5d36876
Mark Jumba <jumbamark@yahoo.com>
2022-03-20 14:16:08 +0300

    Added the create-tweet endpoint

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home_view, tweet_detail_view, tweets_list_view
+from .views import home_view, tweet_detail_view, tweets_list_view, tweet_create_view
 
 app_name = 'tweets'
 
 urlpatterns = [
     path("", home_view, name="homePage"),
+    path("create-tweet", tweet_create_view, name="tweet"),
     path("tweets/", tweets_list_view, name="tweetsList"),
     path("tweets/<int:tweet_id>", tweet_detail_view, name="tweetDetails"),
 ]
