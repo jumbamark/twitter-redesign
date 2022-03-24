a7ed33b737d7f503d1c831d4cb79b2cf4e78dc17
Mark Jumba <jumbamark@yahoo.com>
2022-03-24 15:23:16 +0300

    Associated users to tweets

--- tweets/models.py
+++ tweets/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.conf import settings
 import random
+
+# Referencing a built-in django feature for the User model
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
 class Tweet(models.Model):
     # id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # a user can have many tweets
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to="images/", blank=True, null=True)
 
