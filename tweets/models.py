6721b93dfdb8228892e4cbc6d33ee56bfaa81c01
Mark Jumba <jumbamark@yahoo.com>
2022-03-24 16:25:53 +0300

    Added string representatio to Tweet model

--- tweets/models.py
+++ tweets/models.py
@@ -11,7 +11,9 @@ class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)  # a user can have many tweets
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to="images/", blank=True, null=True)
-
+    
+    def __str__(self):
+        return self.content
     class Meta:
         ordering = ['-id']
 
