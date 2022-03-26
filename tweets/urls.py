f62955b790d1c054de9cf9037b00b3812f3da026
Mark Jumba <jumbamark@yahoo.com>
2022-03-26 12:31:04 +0300

    Added the delete view url

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-from .views import home_view, tweet_detail_view, tweets_list_view, tweet_create_view
+from .views import (
+    home_view, 
+    tweet_detail_view, 
+    tweets_list_view, 
+    tweet_create_view, 
+    tweet_delete_view,
+)
 
 app_name = 'tweets'
 
 urlpatterns = [
     path("", home_view, name="homePage"),
-    path("create-tweet", tweet_create_view, name="tweet"),
+    path("create-tweet", tweet_create_view, name="tweet-create"),
     path("tweets/", tweets_list_view, name="tweetsList"),
-    path("tweets/<int:tweet_id>", tweet_detail_view, name="tweetDetails"),
+    path("tweets/<int:tweet_id>", tweet_detail_view, name="tweet-detail"),
+    path("api/tweets/<int:tweet_id>/delete", tweet_delete_view, name="tweet-delete"),
 ]
