93b766462f69c303d3ced7f915f70f2e4cf0abf5
Mark Jumba <jumbamark@yahoo.com>
2022-03-27 00:40:04 +0300

    Created a serializer for data that comes through for actions

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from .models import Tweet
 
 MAX_TWEET_LENGTH = settings.MAX_TWEET_LENGTH
-
+TWEET_ACTION_OPTIONS = settings.TWEET_ACTION_OPTIONS
 class TweetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tweet
@@ -12,4 +12,14 @@ class TweetSerializer(serializers.ModelSerializer):
     def validate_conten(self, value):
         if len(value) > MAX_TWEET_LENGTH:
             raise serializers.ValidationError("This tweet is too long")
+        return value
+
+class TweetActionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    action = serializers.CharField()
+
+    def validate_action(self, value):
+        value = value.lower().strip()
+        if not value in TWEET_ACTION_OPTIONS:
+            raise serializers.ValidationError("This is not a valid action for tweets")
         return value
