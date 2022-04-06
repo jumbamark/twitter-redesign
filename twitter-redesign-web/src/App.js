8bb100d7d1a0bfeb83f8806ada4ba2b28147f5a1
Mark Jumba <jumbamark@yahoo.com>
2022-04-06 14:48:16 +0300

    Ajax lookup via xhr in Reactjs

--- twitter-redesign-web/src/App.js
+++ twitter-redesign-web/src/App.js
@@ -1,36 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
 import {useState, useEffect} from 'react';
 
+
+
+const loadTweets = function (callback) {
+  // using  XMLHttpRequest to issue HTTP requests-to exchange data between the site and a server.
+  const xhr = new XMLHttpRequest();
+  const method = "GET";
+  const url = "http://127.0.0.1:8000/api/tweets/";
+  const responseType = "json";
+
+  // specitying what type of data the response contains
+  xhr.responseType = responseType;
+
+  // initializing requests
+  xhr.open(method, url);
+
+  // function called when xhr transaction completes successfully.
+  xhr.onload = () => {
+    // Do something with the retrieved data ( found in xhr.response )
+    // console.log(xhr.response);
+    callback(xhr.response, xhr.status)
+  };
+
+  xhr.onerror = (e) => {
+    console.log(e);
+    callback({"message": "The request was an error"}, 400)
+  }
+
+  //  sending the request to the server
+  xhr.send();
+};
+
+
 function App() {
   const [tweets, setTweets] = useState([])
-  const performLookup = () => {
-    
-    // do my lookup
-    const tweetItems = [{"content": 123}, {"content": "Hello world"}]
-    setTweets(tweetItems)
-  }
-  useEffect(performLookup, []) 
+
+  useEffect(() => {
+    const myCallback = (response, status) => {
+      console.log(response, status)
+      if (status === 200) {
+        setTweets(response);
+      } else {
+        alert("There was an error")
+      }
+    }
+    loadTweets(myCallback)
+  }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>{tweets.map((tweet, index) => {
-          return <li>{tweet.content}</li>
-        })}</p>
-      </header>
+    <div className="app">
+      <p>
+        {tweets.map((tweet, index) => {
+          return <li>
+            {tweet.content}
+          </li>
+        })}
+      </p>
     </div>
   );
 }
