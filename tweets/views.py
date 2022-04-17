f6156ec45a15d16e8d77f3bc2bcb9226e6892c58
Mark Jumba <jumbamark@yahoo.com>
2022-04-17 18:27:20 +0300

    Modified authentication

--- tweets/views.py
+++ tweets/views.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework.authentication import SessionAuthentication
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from .serializers import TweetCreateSerializer, TweetSerializer, TweetActionSerializer
 from .models import Tweet
@@ -19,13 +19,12 @@ def home_view(request, *args,**kwargs):
 
 # tweet create view based on serializers
 @api_view(['POST'])
-@authentication_classes([SessionAuthentication])
+# @authentication_classes([SessionAuthentication, BasicAuthentication])
 @permission_classes([IsAuthenticated]) # if they are authenticated they'll have access to this view otherwise they won't
 def tweet_create_view(request, *args, **kwargs):
     serializer = TweetCreateSerializer(data=request.POST)
     if serializer.is_valid(raise_exception=True):
-        obj = serializer.save(user=request.user)
-        print(obj)
+        serializer.save(user=request.user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
