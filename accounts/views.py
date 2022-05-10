9e9f4ae5fd4dabc7226b590e73b455d2456fade1
Mark Jumba <jumbamark@yahoo.com>
2022-05-10 11:52:44 +0300

    Using your own standard form for authentication - If for instance you created your own form

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 def login_view(request, *args, **kwargs):
-    form = AuthenticationForm(request, data=request.POST or None)
+    form = MyModelForm(request.POST or None)
     if form.is_valid():
-        user_ = form.get_user()
+        username = form.cleaned_data.get("username")
+        user_ = authenticate(username, password)
         login(request, user_)
         return redirect("/")
     return render(request, "form.html", {"Form": form})
