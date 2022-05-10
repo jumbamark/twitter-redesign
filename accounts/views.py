859245ed5c970edde6fba19e8d13fb91653020a8
Mark Jumba <jumbamark@yahoo.com>
2022-05-10 11:46:58 +0300

    using the built-in django AuthenticationForm

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout
 
 # Create your views here.
+def login_view(request, *args, **kwargs):
+    form = AuthenticationForm(request, data=request.POST or None)
+    if form.is_valid():
+        user_ = form.get_user()
+        login(request, user_)
+        return redirect("/")
+    return render(request, "form.html", {"Form": form})
