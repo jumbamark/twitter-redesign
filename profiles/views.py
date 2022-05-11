900436a3d8e3c8275e166c5b06962ed988750e43
Mark Jumba <jumbamark@yahoo.com>
2022-05-11 17:48:28 +0300

    Handling Profile Does Not Exists

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.http import Http404
+
+from .models import Profiles
 
 # Create your views here.
 def profile_detail_view(request, username, *args, **kwargs):
     # get the profile for the passed username
-    return render(request, "profiles/detail.html", {"username": username})
+    qs = Profiles.objects.filter(user__username=username)
+    if not qs.exists():
+        raise Http404
+    profile_obj = qs.first()
+    context = {
+        "username": username,
+        "profile": profile_obj
+    }
+    return render(request, "profiles/detail.html", context)
