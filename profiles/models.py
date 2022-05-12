79e71f59ca3787f78f05aa5cb98e071aa486162b
Mark Jumba <jumbamark@yahoo.com>
2022-05-12 15:45:58 +0300

    Followers and Following

--- profiles/models.py
+++ profiles/models.py
@@ -4,10 +4,28 @@ from django.db.models.signals import post_save
 
 User = settings.AUTH_USER_MODEL
 # Create your models here.
+class FollowerRelation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     location = models.CharField(max_length=220, null=True, blank=True)
     bio = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True) # as soon as it saved to the database
+    updated = models.DateTimeField(auto_now=True) # the most recent time it was saved
+    followers = models.ManyToManyField(User, related_name="following", blank=True)
+    """
+    project_obj = Profile.objects.first()
+    project_obj.followers.all() -> All users following this profile
+    user.following.all() -> All users I follow
+    """
+
+    def __str__(self):
+        return self.user.first_name + " " + self.user.last_name
 
 def user_did_save(sender, instance, created, *args, **kwargs):
     if created:
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.contrib.auth import get_user_model
+from .models import Profile
+
+User = get_user_model()
+
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        self.userA = User.objects.create_user(username='testuser', password='testpasswd')
+        self.userB = User.objects.create_user(username='test', password='passwd')
+    
+    def test_profile_created_via_signal(self):
+        """
+        Test that a profile is created automatically when a user is created
+        """
+        queryset = Profile.objects.all()
+        self.assertEqual(queryset.count(), 2)
+
+    def test_following(self):
+        first = self.userA
+        second = self.userB
+        first.profile.followers.add(second) # adds second user to the followers of the first user
+        second_user_following_whom = second.following.all()
+        qs = second_user_following_whom.filter(user=first)
+        self.assertTrue(qs.exists())
+        first_user_following_no_one = first.following.all()
+        self.assertFalse(first_user_following_no_one.exists())
