286dc755be6b57ae6c15738323322d447aa8fa77
Mark Jumba <jumbamark@yahoo.com>
2022-05-18 06:19:06 +0300

    adding the user id and timestamp on tweets

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -5,10 +5,11 @@ from .models import Tweet
 MAX_TWEET_LENGTH = settings.MAX_TWEET_LENGTH
 TWEET_ACTION_OPTIONS = settings.TWEET_ACTION_OPTIONS
 class TweetCreateSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField(read_only=True)
     likes = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Tweet
-        fields = ['id', 'content', 'likes',]
+        fields = ['user' ,'id', 'content', 'likes', 'timestamp']
 
     def get_likes(self, obj):
         return obj.likes.count()
@@ -17,6 +18,9 @@ class TweetCreateSerializer(serializers.ModelSerializer):
         if len(value) > MAX_TWEET_LENGTH:
             raise serializers.ValidationError("This tweet is too long")
         return value
+
+    def get_user(self, obj):
+        return obj.user.id
 
 class TweetActionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
@@ -32,11 +36,15 @@ class TweetActionSerializer(serializers.Serializer):
 
 # Read only serializer because of retweeting
 class TweetSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField(read_only=True)
     likes = serializers.SerializerMethodField(read_only=True)
     parent = TweetCreateSerializer(read_only=True)
     class Meta:
         model = Tweet
-        fields = ['id', 'content', 'likes', 'is_retweet', 'parent']
+        fields = ['user', 'id', 'content', 'likes', 'is_retweet', 'parent', 'timestamp']
 
     def get_likes(self, obj):
         return obj.likes.count()
+    
+    def get_user(self, obj):
+        return obj.user.id
