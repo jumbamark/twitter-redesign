2c34cb581b48e6b4f77d577faf07be456b3b8c4a
Mark Jumba <jumbamark@yahoo.com>
2022-05-20 13:41:16 +0300

    combining the profile views in the api folder

--- profiles/api/urls.py
+++ profiles/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .view import user_follow_view, profile_detail_api_view
+from .view import  profile_detail_api_view
 
 
 """
@@ -11,5 +11,5 @@ app_name = "profiles"
 
 urlpatterns = [
     path("<str:username>", profile_detail_api_view),
-    path("<str:username>/follow/", user_follow_view),
+    path("<str:username>/follow/", profile_detail_api_view),
 ]
--- profiles/api/view.py
+++ profiles/api/view.py
@@ -13,43 +13,57 @@ from ..serializers import PublicProfileSerializer
 User = get_user_model()
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
+
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
-def user_follow_view(request, username, *args, **kwargs):
-    current_user = request.user
-    user_to_follow_qs = User.objects.filter(username=username)
-    if current_user.username == username:
-        my_followers = current_user.profile.followers.all()
-        return Response({"count": my_followers.count(), "message": "You cannot follow yourself"}, status=status.HTTP_400_BAD_REQUEST)
-    if user_to_follow_qs.exists() == False:   #if not user_to_follow.qs.exists
-        return Response({"error": "User does not exist"}, status=status.HTTP_404_NOT_FOUND)
-    user_to_follow = user_to_follow_qs.first()
-    profile = user_to_follow.profile
-    data = {}
-    try:
-        data = request.data
-    except:
-        pass
-    # data = request.data or {}
-    print(data)
-    action = data.get("action")
-    if action == "follow":
-        profile.followers.add(current_user)
-    elif action == "unfollow":
-        profile.followers.remove(current_user)
-    else:
-        pass
-    # current_followers_qs = profile.followers.all()
-    data = PublicProfileSerializer(instance=profile, context={"request":request})
-    return Response(data.data, status=status.HTTP_200_OK)
-
-
-@api_view(['GET'])
 def profile_detail_api_view(request, username, *args, **kwargs):
     # get the profile for the passed username
     qs = Profile.objects.filter(user__username=username)
     if not qs.exists():
         return Response({"detail":"User not found"}, status=status.HTTP_404_NOT_FOUND)
     profile_obj = qs.first()
-    data = PublicProfileSerializer(instance=profile_obj, context={"request": request})
-    return Response(data.data, status=status.HTTP_200_OK)
+    data = request.data or {}
+    if request.method == "POST":
+        current_user = request.user
+        action = data.get("action")
+        if profile_obj.user != current_user:
+            if action == "follow":
+                profile_obj.followers.add(current_user)
+            elif action == "unfollow":
+                profile_obj.followers.remove(current_user)
+            else:
+                pass
+    serializer = PublicProfileSerializer(instance=profile_obj, context={"request": request})
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+# @api_view(['GET', 'POST'])
+# @permission_classes([IsAuthenticated])
+# def user_follow_view(request, username, *args, **kwargs):
+#     current_user = request.user
+#     user_to_follow_qs = User.objects.filter(username=username)
+#     if current_user.username == username:
+#         my_followers = current_user.profile.followers.all()
+#         return Response({"count": my_followers.count(), "message": "You cannot follow yourself"}, status=status.HTTP_400_BAD_REQUEST)
+#     if user_to_follow_qs.exists() == False:   #if not user_to_follow.qs.exists
+#         return Response({"error": "User does not exist"}, status=status.HTTP_404_NOT_FOUND)
+#     user_to_follow = user_to_follow_qs.first()
+#     profile = user_to_follow.profile
+#     data = {}
+#     try:
+#         data = request.data
+#     except:
+#         pass
+#     # data = request.data or {}
+#     print(data)
+#     action = data.get("action")
+#     if action == "follow":
+#         profile.followers.add(current_user)
+#     elif action == "unfollow":
+#         profile.followers.remove(current_user)
+#     else:
+#         pass
+#     # current_followers_qs = profile.followers.all()
+#     data = PublicProfileSerializer(instance=profile, context={"request":request})
+#     return Response(data.data, status=status.HTTP_200_OK)
+
